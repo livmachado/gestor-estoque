fab195e09bd59f4664fcd8c1fbdae86d678144c3
Livia Machado <liviamcampos98@gmail.com>
2025-06-24 22:27:19 -0300

    Integrando rota de produto ao index.ts

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,13 +1,12 @@
 import express from 'express';
+import productRoutes from './routes/product.routes';
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use('/products', productRoutes);
 
-app.get('/', (req, res) => {
-  res.send('API do Gestor de Estoque rodando!');
-});
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
