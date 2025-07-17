000941a90a0ad85161254f86a963ac8a412904e8
Livia Machado <liviamcampos98@gmail.com>
2025-07-16 21:37:19 -0300

    criando rota na sidebar

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,22 @@
+import { Route, Routes } from "react-router-dom"
+import { SideBar } from "./components/SideBar"
+
+//Pages
 import ProductsPage from "./pages/ProductsPage"
+import TransactionsPage from "./pages/TransactionsPage"
+// import TransactionsPage from "./pages/TransactionsPage"
 
 function App() {
 
   return (
-    <div>
-      <ProductsPage/>
-
+    <div className="flex">
+      <SideBar />
+      <main className="bg-blue-50 p-10 w-full">
+        <Routes>
+          <Route path="/produtos" element={<ProductsPage />} />
+          <Route path="/transicoes" element={<TransactionsPage />} />
+        </Routes>
+      </main>
     </div>
   )
 }
