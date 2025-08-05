b487574e6909666597d12ba1e58a2f276b7bf518
Livia Machado <liviamcampos98@gmail.com>
2025-08-04 23:37:31 -0300

    adicionando cors

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -2,12 +2,15 @@ import express from 'express';
 import productRoutes from './routes/product.routes';
 import transactionRoutes from './routes/transaction.routes';
 import reportRoutes from './routes/report.routes'
+import cors from "cors";
 
 
 const app = express();
-const PORT = 3000;
 
+app.use(cors({ origin: "*" }))
 app.use(express.json());
+
+const PORT = 3000;
 app.use('/products', productRoutes);
 app.use('/transaction', transactionRoutes);
 app.use('/report', reportRoutes);
