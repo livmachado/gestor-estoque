07150257f79a26146a710bef324c2ce039e74681
Livia Machado <liviamcampos98@gmail.com>
2025-08-04 23:37:56 -0300

    testando

--- front-end/src/pages/TransactionsPage.tsx
+++ front-end/src/pages/TransactionsPage.tsx
@@ -5,8 +5,81 @@ import TransactionTable from "../components/TransactionsTable";
 import ModalTransaction from "../components/ModalTransaction";
 import PrimaryButton from "../components/PrimaryButton";
 import Breadcrumb from "../components/Breadcrumb";
+import SearchInput from "../components/SearchInput";
 
 const transactionsData = [
+  {
+    id: 1,
+    product: "Mate Caseiro",
+    productId: 1,
+    type: "in",
+    quantity: 2,
+    date: "07/07/24 08:06:52",
+  },
+  {
+    id: 1,
+    product: "Muzzarella",
+    productId: 12,
+    type: "out",
+    quantity: 1000,
+    date: "07/07/24 08:06:52",
+  },
+  {
+    id: 2,
+    product: "Croissant",
+    productId: 7,
+    type: "out",
+    quantity: 9000,
+    date: "06/07/24 18:51:29",
+  },
+  {
+    id: 1,
+    product: "Mate Caseiro",
+    productId: 1,
+    type: "in",
+    quantity: 2,
+    date: "07/07/24 08:06:52",
+  },
+  {
+    id: 1,
+    product: "Muzzarella",
+    productId: 12,
+    type: "out",
+    quantity: 1000,
+    date: "07/07/24 08:06:52",
+  },
+  {
+    id: 2,
+    product: "Croissant",
+    productId: 7,
+    type: "out",
+    quantity: 9000,
+    date: "06/07/24 18:51:29",
+  },
+  {
+    id: 1,
+    product: "Mate Caseiro",
+    productId: 1,
+    type: "in",
+    quantity: 2,
+    date: "07/07/24 08:06:52",
+  },
+  {
+    id: 1,
+    product: "Muzzarella",
+    productId: 12,
+    type: "out",
+    quantity: 1000,
+    date: "07/07/24 08:06:52",
+  },
+  {
+    id: 2,
+    product: "Croissant",
+    productId: 7,
+    type: "out",
+    quantity: 9000,
+    date: "06/07/24 18:51:29",
+  },
   {
     id: 1,
     product: "Mate Caseiro",
@@ -36,6 +109,21 @@ const transactionsData = [
 export default function TransactionsPage() {
   const [isModalTransactionOpen, setIsModalTransactionOpen] =
     useState<boolean>(false);
+    const [search, setSearch] = useState<string>("");
+    
+      const filteredTransactions = transactionsData.filter((transaction) => {
+        const s = search
+          .normalize("NFD")
+          .replace(/[\u0300-\u036f]/g, "")
+          .toLowerCase();
+        const name = transaction.product
+          .normalize("NFD")
+          .replace(/[\u0300-\u036f]/g, "")
+          .toLowerCase();
+        const id = transaction.productId.toString();
+    
+        return name.includes(s) || id.includes(s);
+      });
 
   return (
     <div className="">
@@ -52,8 +140,9 @@ export default function TransactionsPage() {
         />
       </div>
       <hr className="my-4 border-t border-gray-300" />
+      <SearchInput value={search} onChange={setSearch} />
       <div className="flex justify-center">
-        <TransactionTable transactions={transactionsData} />
+        <TransactionTable transactions={filteredTransactions} />
       </div>
       <ModalTransaction
         isOpen={isModalTransactionOpen}
